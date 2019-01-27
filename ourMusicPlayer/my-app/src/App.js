39bdbd3b654eeabb5a1474af539807d20eea9f67
Greyson Wang <greysonw@sfu.ca>
2019-01-26 18:01:02 -0800

    add spawn of python

--- ourMusicPlayer/my-app/src/App.js
+++ ourMusicPlayer/my-app/src/App.js
@@ -25,4 +25,35 @@ class App extends Component {
   }
 }
 
+const express = require('express')
+const app = express()
+
+let runPy = new Promise(function(success, nosuccess) {
+
+    const { spawn } = require('child_process');
+    const pyprog = spawn('python', ['./../spotify_query.py']);
+
+    pyprog.stdout.on('data', function(data) {
+
+        success(data);
+    });
+
+    pyprog.stderr.on('data', (data) => {
+
+        nosuccess(data);
+    });
+});
+
+app.get('/', (req, res) => {
+
+    res.write('welcome\n');
+
+    runPy.then(function(fromRunpy) {
+        console.log(fromRunpy.toString());
+        res.end(fromRunpy);
+    });
+})
+
+app.listen(4000, () => console.log('Application listening on port 4000!'))
+
 export default App;
