0b078a3dc2e682400ce3a89fa0423540365fa556
Greyson Wang <greysonw@sfu.ca>
2019-01-26 19:30:42 -0800

    spotify

--- ourMusicPlayer/my-app/src/spotify_query.py
+++ ourMusicPlayer/my-app/src/spotify_query.py
@@ -1,13 +1,19 @@
 import spotipy
+import spotipy.util as util
+from spotipy.oauth2 import SpotifyClientCredentials
 
-birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
-spotify = spotipy.Spotify()
+#username = "ozujzpuiqgx8oo3k0kzj4cnk1"
+#scope = 'user-library-read'
 
-results = spotify.artist_albums(birdy_uri, album_type='album')
-albums = results['items']
-while results['next']:
-    results = spotify.next(results)
-    albums.extend(results['items'])
+client_credentials_manager = SpotifyClientCredentials(client_id='8ec10118123745c98796a0da389e1b35',
+                                                      client_secret='832c43bccfe44e6d90cd3ac66cc7e28b')
+spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-for album in albums:
-    print(album['name'])
+playlists = spotify.user_playlists('spotify')
+while playlists:
+    for i, playlist in enumerate(playlists['items']):
+        print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
+    if playlists['next']:
+        playlists = spotify.next(playlists)
+    else:
+        playlists = None
