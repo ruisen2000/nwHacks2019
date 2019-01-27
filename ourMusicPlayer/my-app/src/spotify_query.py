429d4c996f34ed6f72f095cbdb473a83d11c447c
Greyson Wang <greysonw@sfu.ca>
2019-01-26 20:54:48 -0800

    user auth

--- ourMusicPlayer/my-app/src/spotify_query.py
+++ ourMusicPlayer/my-app/src/spotify_query.py
@@ -2,18 +2,10 @@ import spotipy
 import spotipy.util as util
 from spotipy.oauth2 import SpotifyClientCredentials
 
-#username = "ozujzpuiqgx8oo3k0kzj4cnk1"
-#scope = 'user-library-read'
+username = "ozujzpuiqgx8oo3k0kzj4cnk1"
+token = util.prompt_for_user_token(username, scope='playlist-modify-private,playlist-modify-public', client_id='8ec10118123745c98796a0da389e1b35', client_secret='832c43bccfe44e6d90cd3ac66cc7e28b', redirect_uri='https://localhost:8080')
 
-client_credentials_manager = SpotifyClientCredentials(client_id='8ec10118123745c98796a0da389e1b35',
-                                                      client_secret='832c43bccfe44e6d90cd3ac66cc7e28b')
-spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+if token:
+    sp = spotipy.Spotify(auth=token)
+    print("Hello")
 
-playlists = spotify.user_playlists('spotify')
-while playlists:
-    for i, playlist in enumerate(playlists['items']):
-        print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
-    if playlists['next']:
-        playlists = spotify.next(playlists)
-    else:
-        playlists = None
